1c10589b7cd914d5e70ddd84a5a6160c8c7a084b
Jessica Patricio <jessicacacau@gmail.com>
2024-09-23 20:49:19 -0300

    edit help texts

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,19 +4,19 @@ from django.db import models
 class ContactForm(models.Model):
     name = models.CharField(
         max_length=200,
-        help_text='Your name e.g. John Doe')
+        help_text='The sender\'s name')
     email = models.EmailField(
-        help_text='Your email address e.g. johndoe@email.com')
+        help_text='The sender\'s email address')
     phone = models.CharField(
         max_length=20,
         null=True,
         blank=True,
-        help_text='Your phone number e.g. +12 34 56789-0123')
+        help_text='The sender\'s phone number')
     subject = models.CharField(
         max_length=100,
-        help_text='A brief description of your message')
-    message = models.TextField(help_text='The message you want to send')
-    sent_at = models.DateTimeField(auto_now_add=True)
+        help_text='The subject of the message')
+    message = models.TextField(help_text='The message content')
+    sent_at = models.DateTimeField(auto_now_add=True, help_text='The date and time the message was sent')
 
     def __str__(self):
         return f'[{self.sent_at}] {self.name} ({self.email}): {self.subject}'
